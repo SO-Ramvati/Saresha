983701d2e39f1f438f6a30300242ec1b2089b66d
DevAnk_1121 <newankitcodecarreer@gmail.com>
2025-12-14 12:59:01 +0530

    Delete rules.py

--- rules.py
+++
@@ -1,8 +0,0 @@
-from academic import academic_response
-from personal import personal_response
-
-def get_response(user_input, mode):
-    if mode == "academic":
-        return academic_response(user_input)
-    else:
-        return personal_response(user_input)
